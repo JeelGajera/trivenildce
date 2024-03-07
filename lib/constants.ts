ae9db861091da2333ddecf22e4a6000f8282aa75
mdhruvil <dhruvil1808@gmail.com>
2024-03-07 17:38:00 +0530

    fix: social links

--- lib/constants.ts
+++ lib/constants.ts
@@ -45,23 +45,13 @@ export const MAIN_PAGE_REFLECTIVE_NAV_LINKS = [
 // replace links with original links
 export const FOOTER_SOCIALS = [
   {
-    label: "Twitter",
-    href: "https://twitter.com/triveniLDEC",
-    icon: Twitter,
-  },
-  {
     label: "Instagram",
-    href: "https://www.instagram.com/triveniLDEC/",
+    href: "https://www.instagram.com/triveni_ldce/",
     icon: Instagram,
   },
   {
     label: "LinkedIn",
     href: "https://www.linkedin.com/company/triveni-ldec/",
     icon: Linkedin,
-  },
-  {
-    label: "Facebook",
-    href: "https://www.facebook.com/triveniLDEC/",
-    icon: Facebook,
   },
 ];
